c609330e0cac2573fb0357644ea972a8ea7c203a
Hugo <hverger@wanadoo.fr>
2015-09-27 07:02:25 +0200

    Adjustements for Android
    support compatibility for blur effect under Chrome

--- www/js/index.js
+++ www/js/index.js
@@ -7,14 +7,14 @@ var app = {
         var listeningElement = parentElement.querySelector('.listening');
         var receivedElement = parentElement.querySelector('.received');
 		var backgroundImage = document.getElementById('background-image');
-		var appDiv = document.getElementById('app');
+		var loading = document.getElementById('loading');
 
         listeningElement.setAttribute('style', 'display:none;');
         receivedElement.setAttribute('style', 'display:block;');
-		backgroundImage.setAttribute('style', 'filter: blur(0px);');
+		backgroundImage.setAttribute('style', '-webkit-filter: blur(0px);filter: blur(0px);');
 
 		setTimeout(function(){
-			appDiv.setAttribute('style', 'display:none;');
+			loading.setAttribute('style', 'display:none;');
 		}, 5000);
     }
 };
