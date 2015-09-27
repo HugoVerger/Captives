fa94d1e4db5a51ba88a4d104ad9012f21951cb14
Hugo <hverger@wanadoo.fr>
2015-09-27 02:51:40 +0200

    Test phonegap
    Ajouts de fichiers de base pour tester PhoneGap Build avec git

---
+++ www/js/index.js
@@ -0,0 +1,13 @@
+var app = {
+    initialize: function() {
+        document.addEventListener('click', this.onDeviceReady, false);
+    },
+    onDeviceReady: function() {
+        var parentElement = document.getElementById('deviceready');
+        var listeningElement = parentElement.querySelector('.listening');
+        var receivedElement = parentElement.querySelector('.received');
+
+        listeningElement.setAttribute('style', 'display:none;');
+        receivedElement.setAttribute('style', 'display:block;');
+    }
+};
