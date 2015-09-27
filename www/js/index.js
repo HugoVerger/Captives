c9a8a0b914e01b377dac401c90c2024beebaf1de
Hugo <hverger@wanadoo.fr>
2015-09-27 06:20:38 +0200

    Transition wake up

--- www/js/index.js
+++ www/js/index.js
@@ -1,13 +1,20 @@
 var app = {
     initialize: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
+        document.addEventListener('click', this.onClick, false);
     },
-    onDeviceReady: function() {
+    onClick: function() {
         var parentElement = document.getElementById('deviceready');
         var listeningElement = parentElement.querySelector('.listening');
         var receivedElement = parentElement.querySelector('.received');
+		var backgroundImage = document.getElementById('background-image');
+		var appDiv = document.getElementById('app');
 
         listeningElement.setAttribute('style', 'display:none;');
         receivedElement.setAttribute('style', 'display:block;');
+		backgroundImage.setAttribute('style', 'filter: blur(0px);');
+
+		setTimeout(function(){
+			appDiv.setAttribute('style', 'display:none;');
+		}, 5000);
     }
 };
