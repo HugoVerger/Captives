2811e68a6f2f905396a2e0fcb867128233302719
Hugo <hverger@wanadoo.fr>
2015-12-15 05:20:53 +0100

    added start screen

---
+++ www/js/start.js
@@ -0,0 +1,22 @@
+function load() {
+	percent++;
+	if (percent>100)
+	{
+		$("#loader").css("display", "none");
+		$("#start").css("display", "block");
+		$(document).bind("click", function() {
+			document.location.href='test.html';
+		});
+	}
+	else
+	{
+		$("#percent").text(percent + "%");
+		//On relance la fonction toutes les 40ms.
+		setTimeout('load()',40);
+	}
+}
+
+window.onload = function() {
+	percent = 0;
+	load();
+}
