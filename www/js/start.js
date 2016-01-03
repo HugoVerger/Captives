aed2eef3cdf6d9164a7f72d2635f9ca2b96822c5
Hugo <hverger@wanadoo.fr>
2016-01-03 07:21:54 +0100

    manual merge

--- www/js/start.js
+++ www/js/start.js
@@ -1,36 +1,11 @@
-var intro = "0";
-
-function load() {
-	percent++;
-    if (percent % 20 == 0)
-    {
-        intro = intro ^ 1;
-        document.getElementById('intro').style.opacity = intro;
-    }
-	if (percent == 160)
-	{
-        document.getElementById('digipal').style.height = "40vmin";
-        document.getElementById('logo_holder').style.height = "115px";
-	}
-    if (percent == 165)
-    {
-        document.getElementById('intro').style.opacity = "1";
-        document.getElementById('title').style.opacity = "1";
-        $("#start").css("display", "block");
-		$("#start").bind("click", function() {
-			document.location.href='test.html';
-		});
-    }
-	if (percent > 0)
-	{
-        document.getElementById('logo').style.height = percent + "px";
-		//$("#percent").text(percent + "%");
-		//On relance la fonction toutes les 40ms.
-		setTimeout('load()', 40);
-	}
+function start() {
+	$("#title").css("opacity", "1");
+	$("#start").css("opacity", "1");
+	$("#start").bind("click", function() {
+		document.location.href='test.html';
+	});
 }
 
 window.onload = function() {
-	percent = 0;
-	load();
+	setTimeout('start()', 5000);
 }
