e41d0d98b2fd476b285618c27251a19fc43e0a31
Hugo <hverger@wanadoo.fr>
2016-01-04 10:44:41 +0100

    Added bug video

--- www/js/bug.js
+++ www/js/bug.js
@@ -1,16 +1,14 @@
 setTimeout(function(){
-	$("#main").css("animation", "fadeout 2s linear");
-	setTimeout(function(){
-		$("#main").css("animation", "");
-		$("#main").removeClass("bug");
-		$("#main").addClass("phonecall");
+	$("#video").fadeOut(2000, function()
+	{
+		$("#background-image").css("display", "block");
 		var audio = new Audio('data/ringtone.mp3');
-		audio.volume = 1;
+		audio.volume = 0.5;
 		audio.loop = true;
 		audio.play();
 		$(window).click(function() {
 			localStorage.setItem(1337, 1);
 			document.location.href="chat.html";
 		});
-	}, 4000);
-}, 3000);
+	});
+}, 6000);
